f4c16fdf6fc20782382d0057890b108078dfb923
AEsir777 <lkb2438834817@gmail.com>
2023-02-07 17:42:47 -0500

    optimized reset button and guessing feature \n also the dice class

--- script/script.js
+++ script/script.js
@@ -1,6 +1,25 @@
 var diceImages = ["./images/dice-face-one.png", "./images/dice-face-two.png", "./images/dice-face-three.png", 
     "./images/dice-face-four.png", "./images/dice-face-five.png", "./images/dice-face-six.png"];
 
+class Dice {
+    constructor(probs = [1, 1, 1, 1, 1, 1]) {
+        let cumSum = 0;
+        this.probs = probs.map((sum => value => sum += value)(0));
+    }
+
+    rollDice() {
+        let res =  Math.random() * this.probs[5];
+        
+        for ( let i = 0; i < 6; i++ ) {
+            if ( res <= this.probs[i] ) return i;
+        }
+        return 5;
+    }
+}
+
+const dice1 = new Dice();
+const dice2 = new Dice();
+
 // input prompt for dice probability
 // alert("Let's start the guessing game for two biased dices!");
 
@@ -10,24 +29,24 @@ var diceImages = ["./images/dice-face-one.png", "./images/dice-face-two.png", ".
 }; */
 
 // function for clicking on start guessing
-function rollDice() {
+function startGame() {
     if ( document.getElementById("guess").value === '' ) {
         alert("Please enter the guess!");
         return;
     }
 
-    const dice1 = Math.floor(Math.random() * 6);
-    const dice2 = Math.floor(Math.random() * 6);
+    const diceRes1 = dice1.rollDice();
+    const diceRes2 = dice2.rollDice();
 
-    document.getElementById("dice1").setAttribute("src", diceImages[dice1]);
-    document.getElementById("dice2").setAttribute("src", diceImages[dice2]);
+    document.getElementById("diceRes1").setAttribute("src", diceImages[diceRes1]);
+    document.getElementById("diceRes2").setAttribute("src", diceImages[diceRes2]);
 
     console.log(document.getElementById("guess").value);
 
     var result = '1';
-    if ( dice1 > dice2 ) {
+    if ( diceRes1 > diceRes2 ) {
         document.querySelector("label").innerHTML = "Player 1 Wins!";
-    } else if ( dice1 < dice2 ) {
+    } else if ( diceRes1 < diceRes2 ) {
         document.querySelector("label").innerHTML = "Player 2 Wins!";
         result = '2';
     } else {
@@ -40,18 +59,10 @@ function rollDice() {
     else 
         document.querySelector("h1.neonBorder").innerHTML = "Booo, you lost!";
     
-    document.querySelector("button.start").classList.toggle("isHidden");
-    document.querySelector("select").classList.toggle("isHidden");
-    document.querySelector("button.restart").classList.toggle("isHidden");
+    document.querySelector("button.start").innerHTML = "Guess again?"
 }
 
-
 function reset() {
-    document.querySelector("h1.neonBorder").innerHTML = "Will you win this game?";
-    document.querySelector("label").innerHTML = "Make your guess using the dropdown below";
-
-    document.querySelector("button.start").classList.toggle("isHidden");
-    document.querySelector("select").classList.toggle("isHidden");
-    document.querySelector("button.restart").classList.toggle("isHidden");
+    window.location.reload();
 }
 
